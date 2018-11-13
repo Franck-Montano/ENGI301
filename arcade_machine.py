8b68171e8f17034d30ca612f2f473539d597da61
Franck Montano <fdm3@rice.edu>
2018-11-12 21:20:53 -0600

    Fixed bug caused by using Windows. Added license. Removes extraneous code

--- arcade_machine.py
+++ arcade_machine.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct 30 19:09:15 2018
-
-@author: Franck
-"""
-
-"""
-
 ---------------------------------------------------------------------------
 PocketBeagle Arcade Machine
 ---------------------------------------------------------------------------
+License:   
+Copyright 2017 Octavo Systems, LLC
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--------------------------------------------------------------------------
+Arcade Machine
+
+  Please see README.txt for overview and how to run the game.
+  
+  Please use run_arcade_machine.sh for instructions to auto-run the game at 
+boot
 
 """
 import os
@@ -57,10 +69,6 @@ LEDS                         = [LED0, LED1, LED2, LED3]
 BUZZER                       = "P2_1"           # gpio50 / (1, 18)
 
 NOTES                        =  [262, 329, 392, 529]
-
-# ------------------------------------------------------------------------
-# Global variables
-# ------------------------------------------------------------------------
 
 # ------------------------------------------------------------------------
 # GPIO/ADC access library
@@ -138,7 +146,7 @@ def adc_get(channel):
 # End def
 
 # ------------------------------------------------------------------------
-# Display Class
+# Display Code
 # ------------------------------------------------------------------------
 HEX_DIGITS                  = [0x3f, 0x06, 0x5b, 0x4f,    # 0, 1, 2, 3
                                0x66, 0x6d, 0x7d, 0x07,    # 4, 5, 6, 7
